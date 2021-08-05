b6ea4dec2cd4847426a7a4cb1a3c5ad15ed361f1
vamandeshmukh <vamandeshmukh@gmail.com>
2021-08-05 15:03:19 +0530

    C

--- src/main/java/com/db/spring/demo/aug2021/App.java
+++ src/main/java/com/db/spring/demo/aug2021/App.java
@@ -1,6 +1,7 @@
 package com.db.spring.demo.aug2021;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
@@ -14,10 +15,20 @@ public class App {
 	public static void main(String[] args) {
 
 //	     Employee obj = new Employee();
+//	     Employee obj2 = new Employee();
 //	     obj.work();
 
 		ApplicationContext context = new ClassPathXmlApplicationContext("SpringConfig.xml");
+		
 		Employee emp = context.getBean("employee", Employee.class);
 		emp.work();
+
+//		Employee emp2 = context.getBean("employee", Employee.class);
+//		System.out.println(emp.hashCode());
+//		System.out.println(emp2.hashCode());
+//		System.out.println(emp.equals(emp2));
+				
+		
+		((AbstractApplicationContext) context).close();
 	}
 }
